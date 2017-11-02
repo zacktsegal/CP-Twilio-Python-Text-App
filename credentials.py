80e93e3eafa46521912b42636ae5647ac7dc599a
zacktsegal <33329148+zacktsegal@users.noreply.github.com>
2017-11-02 14:37:41 -0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = "Your Account Sid"
-auth_token = "Your Auth Token"
+account_sid = "AC2384234b32d250c4f3dd767ebd5f9d8d"
+auth_token = "8f3729c02a794f2298b3c4db77ab1e3d"
 my_cell = "+Your Cell Number"
 my_twilio = "+Your Twilio Number"
